1fa2993052bb291e983accfaed8550249253988f
x-amer-ngmx <naguwal@gmail.com>
2017-05-05 17:06:54 +0300

    4445

--- SISHaU.Library/API/model/MainWorkerClass.cs
+++ SISHaU.Library/API/model/MainWorkerClass.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +11,7 @@ namespace SISHaU.Library.API.model
     {
         private string order;
         private bool done = true;
+        private string asss;
         public bool DoWork()
         {
             //123
