8af5363b00693b4cc91be30deafdea8e655b4a5e
umatkot <umatkot@mail.ru>
2017-05-05 23:10:19 +1000

    Фиксирую основной рабочий класс.

---
+++ SISHaU.Library/API/model/MainWorkerClass.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SISHaU.Library.API.model
+{
+    public class MainWorkerClass
+    {
+        private bool done = true;
+        public bool DoWork()
+        {
+            return done;
+        }
+    }
+}
