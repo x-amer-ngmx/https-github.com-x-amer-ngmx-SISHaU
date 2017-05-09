d90b9072a72fa32bc923e5e3b966cf924071ff63
x-amer-ngmx <naguwal@gmail.com>
2017-05-09 21:33:54 +0300

    Rehsarp'ирование

--- SISHaU.Library/Logger.cs
+++ SISHaU.Library/Logger.cs
@@ -5,12 +5,7 @@ namespace SISHaU.Library
 {
     public static class Logger
     {
-        private static ILog _log = LogManager.GetLogger("LOGGER");
-
-        public static ILog Log
-        {
-            get { return _log; }
-        }
+        public static ILog Log { get; } = LogManager.GetLogger("LOGGER");
 
         public static void InitLogger()
         {
