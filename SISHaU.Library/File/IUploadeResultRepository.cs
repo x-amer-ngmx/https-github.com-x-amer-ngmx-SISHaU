80c6924838f36c64015da5d424cd68d72612f07e
x-amer-ngmx <naguwal@gmail.com>
2017-05-10 16:37:46 +0300

    Корректировка фабрики

---
+++ SISHaU.Library/File/IUploadeResultRepository.cs
@@ -0,0 +1,12 @@
+﻿using SISHaU.Library.File.Model;
+using System;
+using System.Collections.Generic;
+
+namespace SISHaU.Library.File
+{
+    public interface IUploadeResultRepository
+    {
+        void Save(UploadeResultModel upload);
+        IList<UploadeResultModel> GetUploadeInfo(Guid uid);
+    }
+}
