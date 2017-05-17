e90151b5d58a64d7078838b55b418e7a0b3b6fb4
x-amer-ngmx <naguwal@gmail.com>
2017-05-17 17:27:40 +0300

    Notificator

---
+++ SISHaU.Adapter/FluentBilder/FEnginerModel/FNotificationModel/INotification.cs
@@ -0,0 +1,10 @@
+﻿namespace SISHaU.Adapter.FluentBilder.FEnginerModel.FNotificationModel
+{
+    public interface INotification
+    {
+        void importNotificationsOfOrderExecution();
+        void importNotificationsOfOrderExecutionCancellation();
+        void importNotificationData();
+        void exportNotificationsOfOrderExecution();
+    }
+}
