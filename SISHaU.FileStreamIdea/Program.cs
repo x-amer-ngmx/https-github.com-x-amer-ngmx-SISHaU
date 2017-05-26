5107d806c163c80cf38a39fc2dd5ec61100c7368
x-amer-ngmx <naguwal@gmail.com>
2017-05-26 14:31:40 +0300

    Оптимизированный новый тестовый метод расщипления файлов...

--- SISHaU.FileStreamIdea/Program.cs
+++ SISHaU.FileStreamIdea/Program.cs
@@ -19,7 +19,8 @@ namespace SISHaU.FileStreamIdea
                 @"D:\test2.zip",
                 @"D:\test3.zip",
                 @"D:\test4.zip",
-                @"D:\test5.zip"
+                @"D:\test5.zip",
+                @"D:\test6.zip"
             };
             var tmpPath = Path.GetDirectoryName(Assembly.GetEntryAssembly().Location);
 
@@ -28,61 +29,85 @@ namespace SISHaU.FileStreamIdea
             foreach (var patch in files)
             {
 
-                var fName = Path.GetFileNameWithoutExtension(patch);
-                var res = new List<ByteDetectorModel>();
-                long sizeFile = 0;
-                using (var file = new FileStream(patch, FileMode.Open, FileAccess.Read, FileShare.Read))
-                {
+                var res = SplitFiles(tmpPath, patch);
 
-                    sizeFile = file.Length;
-                    var modes = file.Length % ConstantModel.MaxPartSize;
-                    var mod = modes == 0;
 
-                    var parts = (int)(file.Length / ConstantModel.MaxPartSize) + 1;
-
-                    
-
-                    var part = 1;
-                    long partTo = 0;
-
-                    while (part <= parts)
-                    {
-
-                        partTo = part == 1 ? 0 : partTo + ConstantModel.MaxPartSize;
-
-                        var to = (int)partTo;
-                        var from = (int)(part != parts ? (to + ConstantModel.MaxPartSize) : file.Length);
-
-                        var buffSize = (int)(part != parts ? ConstantModel.MaxPartSize : file.Length - to);
-
-                        var buffer = new byte[buffSize];
-
-                        var c = file.Read(buffer, 0, buffSize);
-
-                        var pat = $@"{tmpPath}\{fName}.{part}.tmp";
-
-                        using (var tmpFile = new FileStream(pat, FileMode.Create, FileAccess.Write))
-                        {
-                            tmpFile.Write(buffer, 0, buffer.Length);
-                        }
-
-                        res.Add(new ByteDetectorModel { Part = part, From = to, To = from - 1, Patch = pat });
-                        part++;
-                    }
-
-                }
-
-                using (var tmpFile = new FileStream($@"{tmpPath}\{fName}.zip", FileMode.Create, FileAccess.Write))
+                using (var tmpFile = new FileStream($@"{tmpPath}\{Path.GetFileName(patch)}", FileMode.Create, FileAccess.Write))
                 {
                     foreach (var pat in res)
                     {
                         var buff = File.ReadAllBytes(pat.Patch);
-                        //off = pat.Part == 1 ? 0 : off + buff.Length - 1;
 
                         tmpFile.Write(buff, 0, buff.Length);
+
+                        if (pat.Patch.IndexOf(".tmp") > 0) File.Delete(pat.Patch);
                     }
                 }
             }
+        }
+
+        private static IList<ByteDetectorModel> SplitFiles(string tmpPath, string patch)
+        {
+            var result = new List<ByteDetectorModel>();
+            var fName = Path.GetFileNameWithoutExtension(patch);
+
+            using (var file = new FileStream(patch, FileMode.Open, FileAccess.Read, FileShare.Read))
+            {
+
+                var parts = (int)(file.Length / ConstantModel.MaxPartSize) + 1;
+
+                if (parts == 1)
+                {
+                    result = new List<ByteDetectorModel> {
+                        new ByteDetectorModel{
+                            From = 0,
+                            To = file.Length,
+                            Part = 1,
+                            Patch = patch
+                        }
+                    };
+                    file.Dispose();
+                    return result;
+                }
+
+                var part = 1;
+                long partTo = 0;
+
+                while (part <= parts)
+                {
+                    partTo = part == 1 ? 0 : partTo + ConstantModel.MaxPartSize;
+
+                    long from;
+                    var buffSize = 0;
+
+                    if (part != parts)
+                    {
+                        from = partTo + ConstantModel.MaxPartSize;
+                        buffSize = (int)(ConstantModel.MaxPartSize);
+                    }
+                    else
+                    {
+                        from = file.Length;
+                        buffSize = (int)(file.Length - partTo);
+                    }
+
+                    var buffer = new byte[buffSize];
+                    var partSize = file.Read(buffer, 0, buffSize);
+
+                    var pat = $@"{tmpPath}\{file.Length}_{fName}.{part}.tmp";
+
+                    using (var tmpFile = new FileStream(pat, FileMode.Create, FileAccess.Write))
+                    {
+                        tmpFile.Write(buffer, 0, partSize);
+                    }
+
+                    result.Add(new ByteDetectorModel { Part = part, From = partTo, To = from - 1, Patch = pat });
+                    part++;
+                }
+
+            }
+
+            return result;
         }
     }
 }
