2b7ce045b5b8f75cf1f81a1ee3a42232a637bbef
x-amer-ngmx <naguwal@gmail.com>
2017-05-29 17:13:40 +0300

    Анимация в консоле))))

--- SISHaU.FileStreamIdea/Program.cs
+++ SISHaU.FileStreamIdea/Program.cs
@@ -32,11 +32,44 @@ namespace SISHaU.FileStreamIdea
             if (counter >= sequence.Length) counter = 0;
 
             Console.Write(sequence[counter]);
-            Console.SetCursorPosition(Console.CursorLeft - 1, Console.CursorTop);
+            var left = Console.CursorLeft < sequence.Length ? 0 : Console.CursorLeft - sequence.Length;
+            Console.SetCursorPosition(left, Console.CursorTop);
+
+            //System.Threading.Thread.Sleep(100);
         }
     }
     class Program
     {
+        static void ConsoleDraw(IEnumerable<string> lines, int x, int y)
+        {
+            if (x > Console.WindowWidth) return;
+            if (y > Console.WindowHeight) return;
+
+            var trimLeft = x < 0 ? -x : 0;
+            int index = y;
+
+            x = x < 0 ? 0 : x;
+            y = y < 0 ? 0 : y;
+
+            var linesToPrint =
+                from line in lines
+                let currentIndex = index++
+                where currentIndex > 0 && currentIndex < Console.WindowHeight
+                select new
+                {
+                    Text = new String(line.Skip(trimLeft).Take(Math.Min(Console.WindowWidth - x, line.Length - trimLeft)).ToArray()),
+                    X = x,
+                    Y = y++
+                };
+
+            Console.Clear();
+            foreach (var line in linesToPrint)
+            {
+                Console.SetCursorPosition(line.X, line.Y);
+                Console.Write(line.Text);
+            }
+        }
+
         static void Main(string[] args)
         {
 #if DEBUG
@@ -55,6 +88,41 @@ namespace SISHaU.FileStreamIdea
             var tmpPath = Path.GetDirectoryName(Assembly.GetEntryAssembly().Location);
             var result = new List<UploadeResultModel>();
             Console.CursorVisible = false;
+
+            var arr = new[]
+            {
+                @"      ___                       ___                     ___         ___           ___",
+                @"     /\  \          ___        /\  \                   /\  \       /\__\         /\__\",
+                @"    /::\  \        /\  \      /::\  \                  \:\  \     /:/  /        /:/  /",
+                @"   /:/\:\  \       \:\  \    /:/\ \  \             ___ /::\__\   /:/__/        /:/__/",
+                @"  /:/  \:\  \      /::\__\  _\:\~\ \  \           /\  /:/\/__/  /::\__\____   /::\  \ ___",
+                @" /:/__/_\:\__\  __/:/\/__/ /\ \:\ \ \__\          \:\/:/  /    /:/\:::::\__\ /:/\:\  /\__\",
+                @" \:\  /\ \/__/ /\/:/  /    \:\ \:\ \/__/           \::/  /     \/_|:|~~|~    \/__\:\/:/  /",
+                @"  \:\ \:\__\   \::/__/      \:\ \:\__\              \/__/         |:|  |          \::/  /",
+                @"   \:\/:/  /    \:\__\       \:\/:/  /                            |:|  |          /:/  /",
+                @"    \::/  /      \/__/        \::/  /                             |:|  |         /:/  /",
+                @"     \/__/                     \/__/                               \|__|         \/__/",
+                @"               ___           ___           ___                       ___              ",
+                @"              /\  \         /\__\         /\__\          ___        /\__\",
+                @"             /::\  \       /:/  /        /:/  /         /\  \      /::|  |",
+                @"            /:/\:\  \     /:/  /        /:/__/          \:\  \    /:|:|  |",
+                @"           /::\~\:\  \   /:/  /  ___   /::\__\____      /::\__\  /:/|:|  |__",
+                @"          /:/\:\ \:\__\ /:/__/  /\__\ /:/\:::::\__\  __/:/\/__/ /:/ |:| /\__\",
+                @"          \/__\:\ \/__/ \:\  \ /:/  / \/_|:|~~|~    /\/:/  /    \/__|:|/:/  /",
+                @"               \:\__\    \:\  /:/  /     |:|  |     \::/__/         |:/:/  /",
+                @"                \/__/     \:\/:/  /      |:|  |      \:\__\         |::/  /",
+                @"                           \::/  /       |:|  |       \/__/         /:/  /",
+                @"                            \/__/         \|__|                     \/__/",
+            };
+
+            var maxLength = arr.Aggregate(0, (max, line) => Math.Max(max, line.Length));
+            var x = Console.BufferWidth / 2 - maxLength / 2;
+            for (int y = -arr.Length; y < Console.WindowHeight + arr.Length; y++)
+            {
+                ConsoleDraw(arr, x, y);
+                System.Threading.Thread.Sleep(100);
+            }
+
             foreach (var patch in files)
             {
                 Console.WriteLine($"Обработка файла {{{patch}}}");
